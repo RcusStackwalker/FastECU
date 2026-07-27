761003498f003b7b379e6a304e5ec1478484004a
RcusStackwalker <markelovai@gmail.com>
2026-07-27 21:38:19 +0300

    refactor: remove using

--- src/backend/ports/testing/in_memory_resource_bundle_test.cpp
+++ src/backend/ports/testing/in_memory_resource_bundle_test.cpp
@@ -2,13 +2,9 @@
 
 #include <gtest/gtest.h>
 
-using fastecu::ErrorKind;
-using fastecu::InMemoryResourceBundle;
-using fastecu::Result;
-
 TEST(ResourceBundle, ListReturnsAllNames)
 {
-    InMemoryResourceBundle bundle;
+    fastecu::InMemoryResourceBundle bundle;
     bundle.bundles["config"]["fastecu.cfg"] = {'a'};
     bundle.bundles["config"]["menu.cfg"] = {'b'};
     auto names = bundle.list("config");
@@ -18,15 +14,15 @@ TEST(ResourceBundle, ListReturnsAllNames)
 
 TEST(ResourceBundle, ReadUnknownBundleIsInvalidConfig)
 {
-    InMemoryResourceBundle bundle;
+    fastecu::InMemoryResourceBundle bundle;
     auto r = bundle.read("kernels", "missing.bin");
     ASSERT_FALSE(r.has_value());
-    EXPECT_EQ(r.error().kind, ErrorKind::InvalidConfig);
+    EXPECT_EQ(r.error().kind, fastecu::ErrorKind::InvalidConfig);
 }
 
 TEST(ResourceBundle, ReadKnownFileRoundTrips)
 {
-    InMemoryResourceBundle bundle;
+    fastecu::InMemoryResourceBundle bundle;
     bundle.bundles["kernels"]["k.bin"] = {1, 2, 3};
     auto r = bundle.read("kernels", "k.bin");
     ASSERT_TRUE(r.has_value());
