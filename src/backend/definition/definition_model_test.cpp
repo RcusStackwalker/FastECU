0d637ece3cf41de8c285f29f78711497f3c22ae7
RcusStackwalker <markelovai@gmail.com>
2026-07-30 11:15:41 +0300

    test: enforce portable definition model defaults

--- src/backend/definition/definition_model_test.cpp
+++ src/backend/definition/definition_model_test.cpp
@@ -5,10 +5,14 @@
 #include <string>
 
 using fastecu::ErrorKind;
+using fastecu::definition::AxisDefinition;
+using fastecu::definition::CalibrationMap;
 using fastecu::definition::DefinitionCatalog;
 using fastecu::definition::DefinitionFormat;
 using fastecu::definition::DefinitionIndexEntry;
 using fastecu::definition::IdEncoding;
+using fastecu::definition::RomDefinition;
+using fastecu::definition::Scaling;
 
 namespace
 {
@@ -26,6 +30,50 @@ DefinitionIndexEntry entry(std::string definition_id, std::string source,
         .source = std::move(source),
         .parents = std::move(parents),
     };
+}
+
+TEST(DefinitionModelTest, DefaultsPreserveLegacyValueBuiltSemantics)
+{
+    const Scaling scaling;
+    EXPECT_FALSE(scaling.supplied.tracked);
+    EXPECT_FALSE(scaling.supplied.from_byte);
+    EXPECT_FALSE(scaling.supplied.to_byte);
+    EXPECT_FALSE(scaling.supplied.format);
+
+    const AxisDefinition axis;
+    EXPECT_EQ(axis.size, 1U);
+    EXPECT_EQ(axis.from_byte, "x");
+    EXPECT_EQ(axis.to_byte, "x");
+    EXPECT_EQ(axis.start_position, "1");
+    EXPECT_EQ(axis.interval, "1");
+    EXPECT_FALSE(axis.supplied.tracked);
+    EXPECT_FALSE(axis.supplied.static_data);
+
+    const CalibrationMap map;
+    EXPECT_EQ(map.x_size, 1U);
+    EXPECT_EQ(map.y_size, 1U);
+    EXPECT_FALSE(map.swap_xy);
+    EXPECT_FALSE(map.flip_x);
+    EXPECT_FALSE(map.flip_y);
+    EXPECT_EQ(map.start_position, "1");
+    EXPECT_EQ(map.interval, "1");
+    EXPECT_FALSE(map.supplied.tracked);
+    EXPECT_FALSE(map.supplied.stable_id);
+
+    const RomDefinition definition{};
+    EXPECT_TRUE(definition.resolved_sources.empty());
+    EXPECT_TRUE(definition.resolved_definition_ids.empty());
+}
+
+TEST(DefinitionModelTest, PresenceMetadataParticipatesInValueEquality)
+{
+    AxisDefinition original;
+    AxisDefinition supplied = original;
+    supplied.supplied.tracked = true;
+    supplied.supplied.size = true;
+
+    EXPECT_NE(original, supplied);
+    EXPECT_EQ(original, AxisDefinition{});
 }
 
 TEST(DefinitionCatalogTest, FindsEntryByFormatAndDefinitionId)
