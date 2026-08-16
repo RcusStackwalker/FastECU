b9afe4296fc7be0fdd2a0a45fedca8e047fe7191
RcusStackwalker <markelovai@gmail.com>
2026-08-16 15:04:34 +0200

    refactor:remove QtCipher

--- src/ui/desktop/mainwindow.h
+++ src/ui/desktop/mainwindow.h
@@ -36,7 +36,6 @@
 #include <QThread>
 #include <QMutex>
 
-#include "src/algorithms/crypto/qt_cipher.h"
 #include "src/ui/desktop/calibration_maps.h"
 #include "src/ui/desktop/calibration_treewidget.h"
 #include "src/ui/desktop/protocol_select.h"
@@ -329,8 +328,6 @@ class MainWindow : public QMainWindow
     // Connect signals for any flash class and execute ::run() method
     template <typename FLASH_CLASS>
     FLASH_CLASS *connect_signals_and_run_module(FLASH_CLASS *object);
-    QByteArray aes_ecb_test(QByteArray challenge, QByteArray key);
-    void aes_ecb_example();
     void SetComboBoxItemEnabled(QComboBox *comboBox, int index, bool enabled);
     void set_flash_arrow_state();
     void update_protocol_info(int rom_number);
